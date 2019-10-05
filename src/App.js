6cfc8141bba306f5f24a09ee6d545276304e1152
swd54 <swd543@gmail.com>
2019-10-05 19:17:31 +0200

    Changed logic to communicate with backend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { GridGenerator, Hexagon, HexGrid, Layout, Text, HexUtils } from 'react-hexgrid';
+import { Hexagon, HexGrid, HexUtils, Layout, Text } from 'react-hexgrid';
 import "./App.css";
 
 class App extends React.PureComponent {
+
   constructor() {
     super();
     this.SVGwidth = 1100;
@@ -10,77 +11,45 @@ class App extends React.PureComponent {
     this.SVGheight = this.SVGwidth / 2 * 1.732;
     // Set grid size
     this.gridSize = 10;
-    //Set a default state for our app
-    this.getResetState = () => {
-      const hexagons = GridGenerator.hexagon(this.gridSize);
-      return { hexagons, currentIteration: 0 };
-    }
-    this.state = JSON.parse(window.localStorage.getItem('state')) || this.getResetState();
+
+    this.players = { white: 8, black: 7 }
+    this.state = { currentIteration: 0 }
   }
 
-  checkWin = () => {
-    return false;
+  componentDidMount() {
+    fetch('get').then(r => r.json()).then(r => this.setState({ hexagons: r }))
   }
 
-  findPossible = () => {
-    let { hexagons } = this.state;
-    const { currentIteration } = this.state;
-    const selected = hexagons.filter(h => h.selected);
-    // Remove the meta property 'neighbours' for each hex
-    hexagons.filter(h => h.neighbours).forEach(h => delete h.neighbours);
-    if (currentIteration === 0) {
-      selected.forEach(selection => HexUtils.neighbours(selection).forEach(s => hexagons[this.indexOf(s)].possible = true));
+  getPlayerNameFromNumber = (number) => {
+    const x = Object.entries(this.players).filter(v => v[1] == number)
+    return x && x[0][0]
+  }
+
+  handleClick = (hex) => () => {
+    if (!this.checkPossible(hex)) {
+      alert("Mate, that move is illegal.")
     } else {
-      selected.forEach(selection => HexUtils.neighbours(selection).forEach(s => {
-        const index = this.indexOf(s);
-        if (hexagons[index].hasOwnProperty('neighbours')) {
-          hexagons[index].neighbours++;
-        } else {
-          hexagons[index].neighbours = 1;
-        }
-        hexagons[index].possible = hexagons[index].neighbours >= 2 ? true : false;
-      }))
-    }
-    this.setState(hexagons);
-  }
-
-  indexOf = (p) => {
-    return this.state.hexagons.findIndex(h => HexUtils.equals(h, p));
-  }
-
-  handleClick = (index) => () => {
-    let { hexagons, currentIteration } = this.state;
-    if (hexagons[index].selected) {
-      alert("One does not simply break the galactic code by selecting what's already selected.")
-    }
-    else if (currentIteration > 0 && !hexagons[index].possible) {
-      alert("You cant select that, mate.")
-    }
-    else if (this.checkWin()) {
-      alert("We have a winner!")
-    }
-    else {
-      hexagons[index].selected = this.state.currentIteration % 2 === 0 ? 'white' : 'black';
-      // this.checkWin(index) && alert("Hey, we have a winner!");
-      currentIteration++;
-      const newState = { hexagons, currentIteration, previousIndex: index };
-      this.findPossible();
-      this.setState(newState, () => { window.localStorage.setItem('state', JSON.stringify(newState)) });
+      hex.value = this.state.hexagons.player
+      fetch('move', { method: 'PUT', body: JSON.stringify(hex) })
+        .then(r => {
+          r.headers.get("win") && alert(this.getPlayerNameFromNumber(r.headers.get("win")) + " won!")
+          return r.json()
+        }).then(r => this.setState({ hexagons: r }))
     }
   }
 
-  handleUndo = () => {
-    let { hexagons, currentIteration, previousIndex } = this.state;
-    delete hexagons[previousIndex].selected;
-    currentIteration--;
-    this.setState({ hexagons, currentIteration, previousIndex });
-  }
+  handleUndo = () => { }
 
   handleReset = () => {
-    this.setState(this.getResetState(), () => window.localStorage.setItem('state', JSON.stringify(this.getResetState())));
+    fetch('reset').then(r => r.json()).then(r => this.setState({ hexagons: r }))
+  }
+
+  checkPossible = (hex) => {
+    return this.state.hexagons.currentIteration <= 0 || (this.state.hexagons.currentIteration === 1 && hex.value > 0) || (hex.value > 1 && hex.value < 7)
   }
 
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <header className="App-header">
@@ -94,10 +63,10 @@ class App extends React.PureComponent {
         <div style={{ display: 'flex', flexDirection: 'row' }}>
           <div className="Sidebar">
             <p title="I dont get paid for this, so I keep it simple by naming black and white. Not to be confused with racism." style={{ cursor: 'pointer' }}>
-              {(this.state.currentIteration % 2 === 0 ? 'White' : 'Black') + ' to play'}
+              {(this.state.hexagons && this.getPlayerNameFromNumber(this.state.hexagons.player)) + ' to play'}
             </p>
             <p>
-              Iteration {this.state.currentIteration}
+              Iteration {this.state.hexagons && this.state.hexagons.currentIteration}
             </p>
             <button onClick={this.handleUndo}>Undo</button>
             <button onClick={this.handleReset}>New game</button>
@@ -105,20 +74,24 @@ class App extends React.PureComponent {
               <input type="checkbox" name="havannah" onClick={(e) => this.setState({ [e.target.name]: e.target.checked })} />
               <label>Havannah</label>
             </span>
-
+            <span>
+              <input type="checkbox" name="debugFreedom" onClick={(e) => this.setState({ [e.target.name]: e.target.checked })} />
+              <label>Debug freedom</label>
+            </span>
           </div>
           <div className="Game">
             <HexGrid width={this.SVGwidth} height={this.SVGheight}>
               <Layout size={{ x: 3, y: 3 }} flat={false}>
-                {this.state.hexagons.map((hex, i) => (<Hexagon key={i}
+                {this.state.hexagons && this.state.hexagons.cubic.map((hex, i) => (<Hexagon key={i}
                   q={hex.q}
                   r={hex.r}
                   s={hex.s}
-                  className={hex.selected || (hex.possible ? 'possible' : null)}
-                  onClick={this.handleClick(i)}
-                  data={hex}>
-                  {!this.state.havannah ? <Text>{HexUtils.getID(hex)}</Text> :
-                    <Text>{`${String.fromCharCode(65 + hex.q + hex.r + this.gridSize)},${this.gridSize - hex.q - hex.s}`}</Text>}
+                  className={(hex.value === this.players['white'] && 'white') || (hex.value === this.players['black'] && 'black') || (this.checkPossible(hex) ? 'possible' : null)}
+                  onClick={this.handleClick(hex)}
+                  data={hex} >
+                  {!this.state.havannah ? <Text>{HexUtils.getID(hex)} {this.state.debugFreedom && hex.value}</Text> :
+                    <Text>{`${String.fromCharCode(65 + hex.q + hex.r + this.gridSize)},${this.gridSize - hex.q - hex.s}`} {this.state.debugFreedom && hex.value}</Text>}
+                  {/* // <Text>{`${hex.q + hex.r + this.gridSize},${this.gridSize - hex.q - hex.s}`} {this.state.debugFreedom && hex.value}</Text>} */}
                 </Hexagon>))}
               </Layout>
             </HexGrid>
